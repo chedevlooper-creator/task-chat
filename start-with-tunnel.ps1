d3cbc85217b5cc9f04423c30507a5fd3dc3daa6c
isaha <isaha@users.noreply.github.com>
2026-04-26 04:20:59 +0300

    chore: remove hardcoded OpenClaw credentials from dev script

--- start-with-tunnel.ps1
+++ start-with-tunnel.ps1
@@ -1,4 +1,4 @@
 $env:TASK_CHAT_ENABLE_OPENCLAW_AGENT = "1"
-$env:OPENCLAW_GATEWAY_TOKEN = "e2869008d26c5d4d8d39c495fe35377550af2d4723b0afdf"
-$env:OPENCLAW_GATEWAY_URL = "https://creatures-shadows-href-powerseller.trycloudflare.com"
+$env:OPENCLAW_GATEWAY_TOKEN = ""
+$env:OPENCLAW_GATEWAY_URL = ""
 npm run dev
